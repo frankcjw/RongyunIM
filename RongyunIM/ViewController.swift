2e7f152c2f5afb44031000e36caee5e276830d9a
cjw <fk911c@gmail.com>
2015-05-20 16:14:13 +0800

    update

--- RongyunIM/ViewController.swift
+++ RongyunIM/ViewController.swift
@@ -10,18 +10,22 @@ import UIKit
 
 let RONGCLOUD_IM_APPKEY = "0vnjpoadn51tz"
 let SECRET = "BItzA3xnuUY"
-let TOKEN  = "ye/RMvPpwki49cfEhSoFGy2GkrVUQvL0GUEcUhn8by94uP4K0hqkrvA0i3dUobQjVGYZk9bTRtfZPt2ZrHF9NrWALH7ayqzk"
-let TOKEN2 = "wJOlx+BzeiD0xP0vsmWkuS2GkrVUQvL0GUEcUhn8by94uP4K0hqkrqzd6UVwm8XtVGYZk9bTRtfZPt2ZrHF9NjOQE0FNLNKb"
-
+let TOKEN  = "vBnB541sl0nnJqqGeaiel5E/5bdhTP0LFYPGfeRoPvPtx5/dnYqHaoILzj5bBOKs06QzbX6kU9sTsErf6SWVLwmtq504qa7S"
+let TOKEN2 = "yHf6E9oTNu6xoyltsYO1JTTqU1XLHMf/ktCwYhwiyAKlr5v0jl9MHftQzoraSU6KRe6sJjTyt8rf/QJdGczurbtvYC5sLa8n"
+let TOKEN_C = "5WMcHP/0h/WJk1bNvjiyfDTqU1XLHMf/ktCwYhwiyAKlr5v0jl9MHYKczJaabmoPKX88MhQiLJHf/QJdGczurceIyRIwYROT"
 class ViewController: UIViewController ,RCIMUserInfoFetcherDelegagte{
 
+    var targetName = "aaa"
+    var targetId = "asd"
+    var token = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //hello
 //        RCIMClient.sharedRCIMClient()
         getUserInfo()
-        CJWHttpUtils.manager().requestUrl("http://app.cenjiawen.com:8008/yfg?uid=12&name=cjw", param: nil, shouldCache: false, success: { (resp) -> Void in
+        CJWHttpUtils.manager().requestUrl("http://app.cenjiawen.com:8008/yfg?uid=100000&name=cjw", param: nil, shouldCache: false, success: { (resp) -> Void in
             println("\(resp)")
             if let info = resp["result"] as? String {
                 println("\(info)")
@@ -31,6 +35,38 @@ class ViewController: UIViewController ,RCIMUserInfoFetcherDelegagte{
         }) { () -> Void in
             //
         }
+        
+        var bt1 = UIButton(frame: CGRectMake(20, 300, 100, 30))
+        bt1.addTarget(self, action: "user1", forControlEvents: UIControlEvents.TouchUpInside)
+        bt1.setTitle("用户1", forState: UIControlState.Normal)
+        
+        var bt2 = UIButton(frame: CGRectMake(20, 350, 100, 30))
+        bt2.addTarget(self, action: "user2", forControlEvents: UIControlEvents.TouchUpInside)
+        bt2.setTitle("用户2", forState: UIControlState.Normal)
+        
+        self.view.addSubview(bt1)
+        self.view.addSubview(bt2)
+    }
+    
+    func user1(){
+        token = TOKEN
+        targetId = "402880ef4c"
+        targetName = "ccc"
+        connect()
+    }
+    
+    @IBAction func loginUser1(sender: AnyObject) {
+        user1()
+    }
+    @IBAction func loginUser2(sender: AnyObject) {
+        user2()
+    }
+    
+    func user2(){
+        token = TOKEN2
+        targetId = "402880ef4c"
+        targetName = "ccc"
+        connect()
     }
     
     func getUserInfo(){
@@ -43,33 +79,51 @@ class ViewController: UIViewController ,RCIMUserInfoFetcherDelegagte{
         user.userId = userId;
         
         if userId == "402880ef4b" {
-            user.name = "百度";
+            user.name = "bbb";
             //http://avatar.wolaizuo.com/100000_avatar_lOHMcaS8Pv.jpg
             user.portraitUri = "https://ss0.baidu.com/73t1bjeh1BF3odCf/it/u=1756054607,4047938258&fm=96&s=94D712D20AA1875519EB37BE0300C008"
         }else{
-            user.name = "傻逼";
+            user.name = "aaa";
             user.portraitUri = "http://avatar.wolaizuo.com/100000_avatar_lOHMcaS8Pv.jpg";
 
         }
         return completion(user);
     }
 
+    func dissmiss(){
+        conversationVC.dismissViewControllerAnimated(true, completion: { () -> Void in
+            //
+        })
+    }
+    
+    let conversationVC = ChatViewController()
+
     func showChatView(){
-        let conversationVC = ChatViewController()
         conversationVC.conversationType = RCConversationType.ConversationType_PRIVATE //会话类型，这里设置为 PRIVATE 即发起单聊会话。
 //        RCConversationType.ConversationType_PRIVATE
 
-        conversationVC.targetId = "402880ef4a"; // 接收者的 targetId，这里为举例。
-        conversationVC.targetName = "cao"; // 接受者的 username，这里为举例。
-        conversationVC.title = "聊天的名字"; // 会话的 title。
-//        conversationVC.inp
-//        conversationVC.chatSessionInputBarControl.setInputBarType(RCChatSessionInputBarControlType.DefaultType , style: RCChatSessionInputBarControlStyle.Style9)
-//        conversationVC.inp
+        conversationVC.targetId = targetId; // 接收者的 targetId，这里为举例。
+        conversationVC.targetName = targetName; // 接受者的 username，这里为举例。
+        conversationVC.title = targetName; // 会话的 title。
         conversationVC.setMessageAvatarStyle(RCUserAvatarStyle.Cycle)
+        
+        let bar = UIBarButtonItem(title: "取消", style: UIBarButtonItemStyle.Plain, target: self, action: "dissmiss")
+        conversationVC.navigationItem.leftBarButtonItem = bar
+        
         let navi = UINavigationController(rootViewController: conversationVC)
 
         self.presentViewController(navi, animated: true) { () -> Void in
             //
+        }
+    }
+    
+    func connect(){
+        RCIM.sharedKit().initWithAppKey(RONGCLOUD_IM_APPKEY, deviceToken: token)
+        RCIM.sharedKit().connectWithToken(token, success: { (userId) -> Void in
+            println("userId \(userId)")
+            self.showChatView()
+            }) { (status) -> Void in
+                println("status \(status)")
         }
     }
     
@@ -79,15 +133,8 @@ class ViewController: UIViewController ,RCIMUserInfoFetcherDelegagte{
 //        self.presentViewController(vc, animated: true) { () -> Void in
 //            //
 //        }
-        let tk = TOKEN2
 
-        RCIM.sharedKit().initWithAppKey(RONGCLOUD_IM_APPKEY, deviceToken: tk)
-        RCIM.sharedKit().connectWithToken(tk, success: { (userId) -> Void in
-            println("userId \(userId)")
-            self.showChatView()
-            }) { (status) -> Void in
-                println("status \(status)")
-        }
+        
     }
     
     override func didReceiveMemoryWarning() {
